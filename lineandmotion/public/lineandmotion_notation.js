1879abecf98db9a032ab2064dbd4bbf1652ca0f7
elosine <studiotttguttt>
2019-08-05 15:51:13 -0400

    ups

--- lineandmotion/public/lineandmotion_notation.js
+++ lineandmotion/public/lineandmotion_notation.js
@@ -1,308 +1,185 @@
-//ANIMATION /////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
+// GLOBAL VARIABLES ------------------------------------- //
+//TIMING & ANIMATION ENGINE /////////////
 var frmRate = 60.0;
-var delta = 0,
-  lastFrameTimeMs = 0,
-  timestep = 1000 / frmRate,
-  timelineAbs = 0;
-var aniElements = []; //[0]id, [1]gotime, [2]currpos, [3]lastpos
-var pxPerSec = 50.0;
-var pxPerMs = pxPerSec / 1000;
-var test = 0;
-var pxCount_AtRate = 0;
-var frameClockMs = 0;
-var pxCount_cycle = 0;
-var svgNS = "http://www.w3.org/2000/svg";
-
-var currtick = -9999;
-var lasttick = 9999;
-var curves = [];
-
-var notationnow_svg = document.getElementById("notationnow");
-
-function update(delta) {
-  //delta = duration of 1 frame in ms or ms per frame
-  pxCount_AtRate += pxPerMs * delta;
-  pxCount_cycle = pxCount_AtRate % 1000;
-}
-
-var goPxOffset = 300 + 15; //go cursor location + object (circle) radius
-function draw() {
-  drawTimelineObjs();
-  crvfollow(wholecrvpts);
-}
-
-var wholecrvpts = [];
-var myCircle2;
-
-function drawCrvFollow() {
-  myCircle2 = document.createElementNS(svgNS, "circle");
-  myCircle2.setAttributeNS(null, "cx", 0);
-  myCircle2.setAttributeNS(null, "cy", 130);
-  myCircle2.setAttributeNS(null, "r", 8);
-  myCircle2.setAttributeNS(null, "fill", "orange");
-  myCircle2.setAttributeNS(null, "stroke", "none");
-  notationnow_svg.appendChild(myCircle2);
-
-  for (var i = 0; i < curves[0][1].length; i++) {
-    var stptx1 = curves[0][1][i][1][0], //crv#, pathArray, path#, coordArray, : [ stptx, stpty, ctp1x, ctp1y, ctp2x, ctp2y, endptx, endpty ]
-      stpty1 = curves[0][1][i][1][1],
-      ctp1x1 = curves[0][1][i][1][2],
-      ctp1y1 = curves[0][1][i][1][3],
-      ctp2x1 = curves[0][1][i][1][4],
-      ctp2y1 = curves[0][1][i][1][5],
-      endptx1 = curves[0][1][i][1][6],
-      endpty1 = curves[0][1][i][1][7];
-    var mypts = bezierpts(stptx1, stpty1, ctp1x1, ctp1y1, ctp2x1, ctp2y1, endptx1, endpty1);
-    wholecrvpts = wholecrvpts.concat(mypts);
-  }
-}
-var speedPxPerMs = 33/1000;
-function crvfollow(crvpts) {
-  var tick = Math.round(frameClockMs*speedPxPerMs);
-  tick = tick % crvpts.length;
-
-  //increase 10% per cycle //not quite right 
-  if(tick == 0){
-    speedPxPerMs = speedPxPerMs*1.10;
-  }
-
-
-  myCircle2.setAttributeNS(null, "cx", crvpts[tick].x);
-  myCircle2.setAttributeNS(null, "cy", crvpts[tick].y);
-}
-
-// cubic bezier percent is 0-1
-function getBezierXY(t, sx, sy, cp1x, cp1y, cp2x, cp2y, ex, ey) {
-  return {
-    x: Math.pow(1 - t, 3) * sx + 3 * t * Math.pow(1 - t, 2) * cp1x +
-      3 * t * t * (1 - t) * cp2x + t * t * t * ex,
-    y: Math.pow(1 - t, 3) * sy + 3 * t * Math.pow(1 - t, 2) * cp1y +
-      3 * t * t * (1 - t) * cp2y + t * t * t * ey
-  };
-}
-
-//make array of x/y points
-function bezierpts(stptx, stpty, ctp1x, ctp1y, ctp2x, ctp2y, endptx, endpty) {
-  var pts = [];
-  var first = true;
-  var cfpt, oldx;
-  for (var i = 0; i < 1000; i++) {
-    var inc = 0.001 * i;
-    if (first) {
-      cfpt = getBezierXY(inc, stptx, stpty, ctp1x, ctp1y, ctp2x, ctp2y, endptx, endpty);
-      oldx = cfpt.x;
-      pts.push(cfpt);
-      first = false;
-    } else {
-      cfpt = getBezierXY(inc, stptx, stpty, ctp1x, ctp1y, ctp2x, ctp2y, endptx, endpty);
-      if ((cfpt.x - oldx) >= 1.0) {
-        pts.push(cfpt);
-        oldx = cfpt.x;
-      }
-    }
-
-  }
-  return pts;
-}
-
-function drawTimelineObjs() {
-  if (aniElements.length > 0) {
-    for (i = 0; i < aniElements.length; i++) {
-      let tempdom = document.getElementById(aniElements[i][0]);
-
-      var countdown = aniElements[i][1] - pieceClock;
-      if (countdown >= 0) {
-        if (countdown <= (700 / pxPerMs)) {
-          aniElements[i][2] = (countdown * pxPerMs) + goPxOffset; //current position
-          console.log(countdown + ':' + aniElements[i][2]);
-          tempdom.setAttribute('cx', aniElements[i][2]); //set x pos
-          //remove if past 0
-          if (aniElements[i][2] <= -goPxOffset) {
-            removeElement(aniElements[i][0]);
-            aniElements.splice(i, 1);
-          }
-        }
-      } ////   if (countdown >= 0) {
-    }
-  }
-}
-
+var framect = 0;
+var delta = 0.0;
+var lastFrameTimeMs = 0.0;
+var timestep = 1000.0 / frmRate;
 var pieceClock = 0.0;
 var clockadj = 0.0;
+var timeToGo = 3.0;
+var pxPerSec = 100.0;
+var pxPerFrame = pxPerSec / frmRate;
+var pxPerMs = pxPerSec / 1000.0;
+//STATUS BAR ////////////////////////////
+var sb = true;
+var statusbar = document.getElementById('statusbar');
+//SCENE /////////////////////////////////
+var renderer, scene, camera, pointLight, spotLight;
+var fieldLength = 700;
+var fieldWidth = 150;
+//TEMPO FRETS ///////////////////////////////
+var tfgeom, tfmatl, gofret, tfbgeom, tfbmatl, gofretborder;
+var gofretposx = -340;
+var gofretposz = 3;
+var tfs = [];
+// SET UP ----------------------------------------------- //
+function setup() {
+  createScene();
+  requestAnimationFrame(mainLoop);
+}
+// FUNCTION: CREATE SCENE ------------------------------- //
+function createScene() {
+  //Scene Size
+  var WIDTH = 500;
+  var HEIGHT = 300;
+  //Camera Attributes
+  var VIEW_ANGLE = 45;
+  var ASPECT = WIDTH / HEIGHT;
+  var NEAR = 0.1;
+  var FAR = 10000;
+  //Get DOM div
+  var c = document.getElementById("tlcanvas");
+  // create a WebGL renderer, camera, and a scene
+  renderer = new THREE.WebGLRenderer();
+  camera =
+    new THREE.PerspectiveCamera(
+      VIEW_ANGLE,
+      ASPECT,
+      NEAR,
+      FAR);
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x708090);
+  scene.add(camera);
+  //Camera Position
+  camera.position.x = -(fieldLength / 2) - 95;
+  camera.position.z = 120;
+  camera.rotation.y = -60 * Math.PI / 180;
+  camera.rotation.z = -90 * Math.PI / 180;
+  //Start the renderer
+  renderer.setSize(WIDTH, HEIGHT);
+  //Attach the render-supplied DOM element
+  c.appendChild(renderer.domElement);
+  //Set up the playing surface plane
+  var planeLength = fieldLength;
+  var planeWidth = fieldWidth + 50;
+  var planeQuality = 10;
+  var planeMaterial =
+    new THREE.MeshLambertMaterial({
+      color: 'black'
+    });
+  var plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(
+      planeLength * 0.99, // 95% of table width, since we want to show where the ball goes out-of-bounds
+      planeWidth,
+      planeQuality,
+      planeQuality
+    ),
+    planeMaterial
+  );
+  scene.add(plane);
+  plane.receiveShadow = true;
+  //TEMPO FRETS ///////////////////////////////////////////
+  tfgeom = new THREE.CubeGeometry(4, 194, 5);
+  gfmatl = new THREE.MeshBasicMaterial({
+    color: "rgb(255,255,0)"
+  });
+  tfmatl = new THREE.MeshBasicMaterial({
+    color: "rgb(255,128,0)"
+  });
+  tfbgeom = new THREE.EdgesGeometry(tfgeom);
+  tfbmatl = new THREE.LineBasicMaterial({
+    color: 0x000000,
+  });
+  gofret = new THREE.Mesh(tfgeom, gfmatl);
+  gofretborder = new THREE.LineSegments(tfbgeom, tfbmatl);
+  gofretborder.renderOrder = 1; // make sure wireframes are rendered 2nd
+  gofret.position.x = gofretposx;
+  gofret.position.z = gofretposz;
+  gofretborder.position.x = gofretposx;
+  gofretborder.position.z = gofretposz;
+  scene.add(gofret);
+  scene.add(gofretborder);
 
+  for (var i = 0; i < 7; i++) {
+    //Add orange fret and move
+    var tft = new THREE.Mesh(tfgeom, tfmatl);
+    var tfbt = new THREE.LineSegments(tfbgeom, tfbmatl);
+    tfbt.renderOrder = 1;
+    tft.position.x = pxPerSec * i;
+    tft.position.z = gofretposz;
+    tfbt.position.x = pxPerSec * i;
+    tfbt.position.z = gofretposz;
+    tft.name = "tft" + i;
+    tfbt.name = "tfbt" + i;
+    //[boolean:add, tft, tfbt]
+    var ntft = [true, tft, tfbt];
+    tfs.push(ntft);
+  }
+
+  // // create a point light
+  pointLight =
+    new THREE.PointLight(0xF8D898);
+  // set its position
+  pointLight.position.x = -1000;
+  pointLight.position.y = 0;
+  pointLight.position.z = 1000;
+  pointLight.intensity = 2.9;
+  pointLight.distance = 10000;
+  // add to the scene
+  scene.add(pointLight);
+  // add a spot light
+  spotLight = new THREE.SpotLight(0xF8D898);
+  spotLight.position.set(0, 0, 460);
+  spotLight.intensity = 1.5;
+  spotLight.castShadow = true;
+  scene.add(spotLight);
+  // MAGIC SHADOW CREATOR DELUXE EDITION with Lights PackTM DLC
+  renderer.shadowMapEnabled = true;
+}
+// DRAW --------------------------------------------------------- //
+function draw() {
+  //Status Bar ///////////////////////////
+  if (sb) {
+    statusbar.textContent = "Time: " + (pieceClock / 1000).toFixed(2) + " " + "Frame: " + framect.toFixed(1) + " " + "Clock: " + (Date.now() / 1000).toFixed(2);
+  }
+  // MAIN TIMELINE THREE.JS SCENE
+  renderer.render(scene, camera);
+}
+// UPDATE ------------------------------------------------------ //
+function update(timestep) {
+  framect++;
+  //Clock //////////////////////////////////
+  pieceClock += timestep;
+  pieceClock = pieceClock - clockadj;
+  //Tempo FRETS
+  for (var i = 0; i < tfs.length; i++) {
+    if (tfs[i][1].position.x >= gofretposx) {
+      tfs[i][1].position.x -= pxPerFrame;
+      tfs[i][2].position.x -= pxPerFrame;
+    }
+    if (tfs[i][1].position.x < 300) {
+      if (tfs[i][0]) {
+        tfs[i][0] = false;
+        scene.add(tfs[i][1]);
+        scene.add(tfs[i][2]);
+      }
+    }
+    if(tfs[i][1].position.x < gofretposx) {
+      scene.remove(scene.getObjectByName(tfs[i][1].name));
+      scene.remove(scene.getObjectByName(tfs[i][2].name));
+      tfs.splice(i,1);
+    }
+  }
+}
+// ANIMATION ENGINE -------------------------------------------- //
 function mainLoop(timestamp) {
-  pieceClock = timestamp + clockadj;
-  delta += pieceClock - lastFrameTimeMs; //delta = time transpired since last frame
-  lastFrameTimeMs = pieceClock;
-  //timestep = # of ms per frame at the desired framerate
-  //this loop will only update if the time transpired since last frame
-  //is greater or = to the desired timestep
-  //and it will keep the additional time for the next frame
-  //so we keep up with the clock
+  delta += timestamp - lastFrameTimeMs;
+  lastFrameTimeMs = timestamp;
   while (delta >= timestep) {
-    pxCount_AtRate += pxPerMs * timestep;
-    pxCount_cycle = Math.round(pxCount_AtRate) % 1000;
-    frameClockMs += timestep;
+    update(timestep);
     delta -= timestep;
   }
   draw();
   requestAnimationFrame(mainLoop);
 }
-
-// Start things off
-requestAnimationFrame(mainLoop);
-//END ANIMATION /////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
-
-//OSC via websockets /////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
-var socket = io();
-socket.on('mkcirc',
-  function(msg) {
-    mkcirc(msg[0], msg[1], msg[2]);
-  });
-
-socket.on('newtime',
-  function(msg) {
-    //pxCount_AtRate = msg[0];
-    var tsync = msg[0] * 1000.0;
-    clockadj = tsync - window.performance.now();
-    lastFrameTimeMs = tsync;
-    pxCount_AtRate = tsync * pxPerMs;
-    console.log(window.performance.now() + ' : ' + tsync + ' : ' + clockadj);
-  });
-//END OSC via websockets /////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
-
-
-//MAKE SVG ELEMENTS /////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
-
-function mkcirc(id, gotime, cy) {
-  var myCircle = document.createElementNS(svgNS, "circle"); //to create a circle. for rectangle use "rectangle"
-  myCircle.setAttributeNS(null, "id", id);
-  myCircle.setAttributeNS(null, "cx", 20000);
-  myCircle.setAttributeNS(null, "cy", cy);
-  myCircle.setAttributeNS(null, "r", 15);
-  myCircle.setAttributeNS(null, "fill", "Aqua");
-  myCircle.setAttributeNS(null, "stroke", "none");
-
-  document.getElementById("timeline_svg").appendChild(myCircle);
-  aniElements.push([id, gotime, -9999, 9999]);
-}
-
-function removeElement(elementId) {
-  // Removes an element from the document
-  var element = document.getElementById(elementId);
-  element.parentNode.removeChild(element);
-}
-//END MAKE SVG ELEMENTS /////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
-
-//CURVES /////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
-//<path d="M9, 12 C93, 7, 43, 271, 178, 89" stroke="#99ff00" stroke-width="5"/>
-
-drawCrv001(notationnow_svg, 'crv001_1_', 0, 0, 300, 130, "#99ff00", "5");
-
-// Curve Functions /////////////////////////////////////////////////////////
-function drawCrv001(ctx, id, x, y, w, h, clr, strw) {
-  var ns = "http://www.w3.org/2000/svg";
-  var stptx, stpty, ctp1x, ctp1y, ctp2x, ctp2y, endptx, endpty;
-  path = [];
-  var idix = 0;
-
-  stptx = ((0.0 * w) + x);
-  stpty = ((1.0 * h) + y);
-  ctp1x = ((0.1283 * w) + x);
-  ctp1y = ((0.9867 * h) + y);
-  ctp2x = ((0.0302 * w) + x);
-  ctp2y = ((0.2583 * h) + y);
-  endptx = ((0.1567 * w) + x);
-  endpty = ((0.3089 * h) + y);
-  path.push([id + idix, [stptx, stpty, ctp1x, ctp1y, ctp2x, ctp2y, endptx, endpty], 'M' + stptx + ', ' + stpty + ' C' + ctp1x + ', ' + ctp1y + ', ' + ctp2x + ', ' + ctp2y + ', ' + endptx + ', ' + endpty]);
-  idix++;
-  stptx = ((0.1567 * w) + x);
-  stpty = ((0.3089 * h) + y);
-  ctp1x = ((0.2233 * w) + x);
-  ctp1y = ((0.3356 * h) + y);
-  ctp2x = ((0.2906 * w) + x);
-  ctp2y = ((0.6715 * h) + y);
-  endptx = ((0.3750 * w) + x);
-  endpty = ((0.6422 * h) + y);
-  path.push([id + idix, [stptx, stpty, ctp1x, ctp1y, ctp2x, ctp2y, endptx, endpty], 'M' + stptx + ', ' + stpty + ' C' + ctp1x + ', ' + ctp1y + ', ' + ctp2x + ', ' + ctp2y + ', ' + endptx + ', ' + endpty]);
-  idix++;
-  stptx = ((0.3750 * w) + x);
-  stpty = ((0.6422 * h) + y);
-  ctp1x = ((0.5350 * w) + x);
-  ctp1y = ((0.5867 * h) + y);
-  ctp2x = ((0.4850 * w) + x);
-  ctp2y = ((-0.0178 * h) + y);
-  endptx = ((0.5900 * w) + x);
-  endpty = ((0.0178 * h) + y);
-  path.push([id + idix, [stptx, stpty, ctp1x, ctp1y, ctp2x, ctp2y, endptx, endpty], 'M' + stptx + ', ' + stpty + ' C' + ctp1x + ', ' + ctp1y + ', ' + ctp2x + ', ' + ctp2y + ', ' + endptx + ', ' + endpty]);
-  idix++;
-  stptx = ((0.5900 * w) + x);
-  stpty = ((0.0178 * h) + y);
-  ctp1x = ((0.6963 * w) + x);
-  ctp1y = ((0.0538 * h) + y);
-  ctp2x = ((0.6700 * w) + x);
-  ctp2y = ((0.4911 * h) + y);
-  endptx = ((0.8583 * w) + x);
-  endpty = ((0.4022 * h) + y);
-  path.push([id + idix, [stptx, stpty, ctp1x, ctp1y, ctp2x, ctp2y, endptx, endpty], 'M' + stptx + ', ' + stpty + ' C' + ctp1x + ', ' + ctp1y + ', ' + ctp2x + ', ' + ctp2y + ', ' + endptx + ', ' + endpty]);
-  idix++;
-  stptx = ((0.8583 * w) + x);
-  stpty = ((0.4022 * h) + y);
-  ctp1x = ((0.9528 * w) + x);
-  ctp1y = ((0.3576 * h) + y);
-  ctp2x = ((0.8450 * w) + x);
-  ctp2y = ((0.9778 * h) + y);
-  endptx = ((1.0 * w) + x);
-  endpty = ((1.0 * h) + y);
-  path.push([id + idix, [stptx, stpty, ctp1x, ctp1y, ctp2x, ctp2y, endptx, endpty], 'M' + stptx + ', ' + stpty + ' C' + ctp1x + ', ' + ctp1y + ', ' + ctp2x + ', ' + ctp2y + ', ' + endptx + ', ' + endpty]);
-  idix++;
-  for (i = 0; i < path.length; i++) {
-    var pathtemp = document.createElementNS(svgNS, "path");
-    pathtemp.setAttribute('id', path[i][0]);
-    pathtemp.setAttribute('stroke', clr);
-    pathtemp.setAttribute('stroke-width', strw);
-    pathtemp.setAttribute('d', path[i][2]);
-    ctx.appendChild(pathtemp);
-  }
-  curves.push([id, path]);
-  drawCrvFollow();
-}
-//CURVES /////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
-
-//VexFlow Notation /////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////
-VF = Vex.Flow;
-// Create an SVG renderer and attach it to the DIV element named "nframe".
-var div = document.getElementById("nframe")
-var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
-// Configure the rendering context.
-renderer.resize(100, 133);
-var context = renderer.getContext();
-context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
-// Create a stave of width 400 at position 10, 40 on the canvas.
-var stave = new VF.Stave(5, 5, 300);
-stave.addClef("treble");
-var notes = [
-  new VF.StaveNote({
-    clef: "treble",
-    keys: ["c/5"],
-    duration: "w"
-  })
-];
-var voice = new VF.Voice({
-  num_beats: 4,
-  beat_value: 4
-});
-voice.addTickables(notes);
-var formatter = new Vex.Flow.Formatter().
-joinVoices([voice]).format([voice], 290);
-voice.draw(context, stave);
-stave.setContext(context).draw();
---
+++ lnm_archive/lineandmotion_rebuild/3js/Scripts/game.js
@@ -0,0 +1,182 @@
+// ------------------------------------- //
+// ------- GLOBAL VARIABLES ------------ //
+// ------------------------------------- //
+
+// scene object variables
+var renderer, scene, camera, pointLight, spotLight;
+// field variables
+var fieldLength = 700;
+var fieldWidth = 150;
+// ball variables
+var ball;
+var ballSpeed = 2;
+var fgeom, fmatl, fret;
+
+function setup() {
+  createScene();
+  draw();
+}
+
+function createScene() {
+
+  // set the scene size
+  var WIDTH = 500,
+    HEIGHT = 300;
+  // Camera attributes
+  // var VIEW_ANGLE = 70,
+  // var VIEW_ANGLE = 45,
+  var VIEW_ANGLE = 45,
+
+    ASPECT = WIDTH / HEIGHT,
+    NEAR = 0.1,
+    FAR = 10000;
+  var c = document.getElementById("gameCanvas");
+  // create a WebGL renderer, camera
+  // and a scene
+  renderer = new THREE.WebGLRenderer();
+  camera =
+    new THREE.PerspectiveCamera(
+      VIEW_ANGLE,
+      ASPECT,
+      NEAR,
+      FAR);
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x708090);
+  // add the camera to the scene
+  scene.add(camera);
+  // set a default position for the camera
+  // camera.position.x = -(fieldLength / 2) - 83;
+  // camera.position.x = -(fieldLength / 2) - 47;
+  camera.position.x = -(fieldLength / 2) - 95;
+  // camera.position.z = 80;
+  // camera.position.z = 110;
+  camera.position.z = 120;
+
+  camera.rotation.y = -60 * Math.PI / 180;
+  camera.rotation.z = -90 * Math.PI / 180;
+  // start the renderer
+  renderer.setSize(WIDTH, HEIGHT);
+  // attach the render-supplied DOM element
+  c.appendChild(renderer.domElement);
+  // set up the playing surface plane
+  var planeLength = fieldLength;
+  var planeWidth = fieldWidth+50;
+  var planeQuality = 10;
+  // create the plane's material
+  var planeMaterial =
+    new THREE.MeshLambertMaterial({
+      color: 'black'
+    });
+
+  // create the playing surface plane
+  var plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(
+      planeLength * 0.99, // 95% of table width, since we want to show where the ball goes out-of-bounds
+      planeWidth,
+      planeQuality,
+      planeQuality
+    ),
+    planeMaterial
+  );
+
+  scene.add(plane);
+  plane.receiveShadow = true;
+
+  // // set up the sphere vars
+  // lower 'segment' and 'ring' values will increase performance
+  var radius = 7,
+    segments = 6,
+    rings = 6;
+  // // create the sphere's material
+  var sphereMaterial =
+    new THREE.MeshLambertMaterial({
+      color: 0xD43001
+    });
+  // Create a ball with sphere geometry
+  ball = new THREE.Mesh(
+    new THREE.SphereGeometry(
+      radius,
+      segments,
+      rings
+    ),
+    sphereMaterial
+  );
+
+  //Tempo Frets
+  fgeom = new THREE.CubeGeometry(4, 194, 5);
+  fmatl = new THREE.MeshBasicMaterial({
+    color: "rgb(255,255,0)"
+  });
+  fret = new THREE.Mesh(fgeom, fmatl);
+  scene.add(fret);
+  var fposx = -340;
+  var fposz = 3;
+  fret.position.x = fposx;
+  fret.position.z = fposz;
+
+  // wireframe
+  var geo = new THREE.EdgesGeometry(fgeom);
+  var mat = new THREE.LineBasicMaterial({
+    color: 0x000000,
+    linewidth: 4
+  });
+  var wf = new THREE.LineSegments(geo, mat);
+  wf.renderOrder = 1; // make sure wireframes are rendered 2nd
+  scene.add(wf);
+  wf.position.x = fposx;
+  wf.position.z = fposz;
+
+  //Add Red fret and move
+
+
+  // // add the sphere to the scene
+  scene.add(ball);
+  ball.position.x = fieldLength / 2;
+  ball.position.y = (fieldWidth / 2) - 20;
+  // set ball above the table surface
+  ball.position.z = radius;
+  ball.receiveShadow = true;
+  ball.castShadow = true;
+
+  // // create a point light
+  pointLight =
+    new THREE.PointLight(0xF8D898);
+  // set its position
+  pointLight.position.x = -1000;
+  pointLight.position.y = 0;
+  pointLight.position.z = 1000;
+  pointLight.intensity = 2.9;
+  pointLight.distance = 10000;
+  // add to the scene
+  scene.add(pointLight);
+
+  // add a spot light
+  // this is important for casting shadows
+  spotLight = new THREE.SpotLight(0xF8D898);
+  spotLight.position.set(0, 0, 460);
+  spotLight.intensity = 1.5;
+  spotLight.castShadow = true;
+  scene.add(spotLight);
+  // MAGIC SHADOW CREATOR DELUXE EDITION with Lights PackTM DLC
+  renderer.shadowMapEnabled = true;
+}
+
+function draw() {
+  // draw THREE.JS scene
+  renderer.render(scene, camera);
+  // loop draw function call
+  requestAnimationFrame(draw);
+  ballPhysics();
+}
+
+function ballPhysics() {
+  // we can easily notice shadows if we dynamically move lights during the game
+  spotLight.position.x = ball.position.x * 2;
+  spotLight.position.y = ball.position.y * 2;
+  // animate ball if not off table
+  if (ball.position.x >= -fieldLength / 2) {
+    ball.position.x -= ballSpeed;
+  } else {
+    ball.position.z -= ballSpeed;
+  }
+}
